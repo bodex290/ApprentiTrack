6ef2be0088e014790021f7deb817b1aa29a32be1
bodex290 <samuelojeiwa290@gmail.com>
2026-04-22 13:24:52 +0100

    enable vercel speed insights and analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
+    <Analytics />
   </StrictMode>,
 )
